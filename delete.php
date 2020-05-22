e9e1aa15afca29c1562b7690c2a8cdf590502fc3
ademalkan <43451577+ademalkan@users.noreply.github.com>
2020-05-22 23:25:53 +0300

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+$id = $_GET['id'];
+
+$baglan = mysqli_connect('localhost', 'root', '', 'db');
+$veriSil = $baglan->query("DELETE FROM  tablo WHERE id=$id");
+header("location:index.php");
+ ?>
